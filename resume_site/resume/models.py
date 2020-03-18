4518781a336bf11c82a90bf78156a307f03435df
Tyler Stevahn <tylerjay1996@hotmail.com>
2020-03-18 04:46:42 -0500

    first commit

---
+++ resume_site/resume/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class registrationdata(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
